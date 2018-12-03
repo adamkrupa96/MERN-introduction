82960c6f7bd61e87dabff0629059c1c0bfa755cf
Adam <adamkrupa96@gmail.com>
2018-12-03 04:09:49 +0100

    add setup for mongodb. insert new documents

--- server/index.js
+++ server/index.js
@@ -4,9 +4,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// mongoDB setup
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'todo-app';
+
+const client = new MongoClient(dbUrl);
+
+client.connect((error) => {
+    assert.equal(null, error);
+    console.log("Connected successfully to database server");
+
+    const db = client.db(dbName);
+
+    insertDocuments(db, () => {
+        client.close();
+    });
+});
+
+
 // express app setup
 const appHandlers = [
-    cors(), 
+    cors(),
     bodyParser.json()
 ];
 
@@ -19,5 +39,20 @@ app.listen(appPort, () => {
 app.get('/', (request, response) => {
     response
         .status(200)
-        .send(JSON.stringify({'greeting': 'Hello World!' }));
+        .send(JSON.stringify({ 'greeting': 'Hello World!' }));
 });
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        { a: 1 }, { a: 2 }, { a: 3 }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}

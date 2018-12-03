2eeec366651ba8ec3286bf525b97ea6b0264c63e
Adam <adamkrupa96@gmail.com>
2018-12-03 03:42:20 +0100

    add axios client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      greeting: ''
+    }
+  }
+
+  componentDidMount() {
+    this.fetchHelloWorld();
+  }
+
+  async fetchHelloWorld() {
+    const response = await axios.get('http://localhost:8080');
+
+    this.setState({
+      greeting: response.data.greeting
+    });
+  }
+
   render() {
     return (
       <div className="text-center">
         <h1>Hello React</h1>
+
+        <p>Server response: {this.state.greeting}</p>
       </div>
     );
   }
